d9a92f2a227596ac4bc6512f0a5afb320732210d
JungJinHwan <yadot1478@naver.com>
2019-12-15 13:55:39 +0900

    add script

--- myscripts/make_server.sh
+++ myscripts/make_server.sh
@@ -1,20 +1,20 @@
 # Build Docker Image
-docker build -t simple_server_with_docker_img .
+docker build -t simple_server .
 
 # Create Docker Network
 docker network create simple_network 2>/dev/null
 
 # Remove Container
-docker rm -f $(docker ps -a | grep "simple_server_with_docker_container" | awk "{print \$1}")
+docker rm -f $(docker ps -a | grep "simple_server" | awk "{print \$1}")
 
 # New Container
-docker run --name simple_server_with_docker_container \
+docker run --name simple_server \
   -itd \
   -p 80:8080 \
   -m=2g \
   --network simple_network \
   --restart unless-stopped \
-  simple_server_with_docker_img
+  simple_server
 
 # Clean Dummy Docker Image
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
